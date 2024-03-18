fc46921f49af48f100bf7ca023901fd168da51bf
Jonah Cote <jonah@greenmtnit.com>
2024-03-18 12:02:20 -0400

    Initial commit, get-physicaldisk based

--- diskhealthmonitoring.ps1
+++ diskhealthmonitoring.ps1
@@ -1,1 +1,22 @@
-# Hello world!
+# Get information about physical disks
+$disks = Get-PhysicalDisk
+
+# Loop through each disk and display relevant information
+foreach ($disk in $disks) {
+    # Disk information for debugging
+    # Write-Host "DeviceID: $($disk.DeviceID)"
+    # Write-Host "OperationalStatus: $($disk.OperationalStatus)"
+    # Write-Host "HealthStatus: $($disk.HealthStatus)"
+    
+    # Check if the disk is unhealthy
+    if (!($disk.HealthStatus -eq "Healthy")) {
+      Write-Host "Disk $($disk.FriendlyName), $($disk.MediaType):"
+      Write-Host "Disk $($disk.DeviceID) is unhealthy! Take corrective action."
+      # Add RMM alert call here
+    }
+    else {
+      Write-Host "Disk $($disk.FriendlyName), $($disk.MediaType):"
+      Write-Host "Disk $($disk.DeviceID) is healthy."
+    }
+}
+
