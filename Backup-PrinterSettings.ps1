854b0154d8ad4b58b8edefeb0823ae6b590788e9
Timothy West <tim@greenmtnit.com>
2024-04-10 15:16:13 -0400

    Added Backup-PrinterSettings.ps1

---
+++ Backup-PrinterSettings.ps1
@@ -0,0 +1,56 @@
+#####################################################################
+$BackupDriver = $true
+#####################################################################
+
+if (-not (Get-Module PSWriteHTML)){
+    Install-Module -Name PSWriteHTML -AllowClobber -Force
+}
+
+Import-Module -Name PSWriteHTML
+Import-Module -Name PrintManagement
+
+# Get current timestamp for backup directory
+$Timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+$BackupDirectory = "C:\Temp\PrinterBackup_$Timestamp"
+
+# Create the backup directory
+if (-Not (Test-Path $BackupDirectory -ErrorAction SilentlyContinue)) {
+    New-Item $BackupDirectory -ItemType Directory | Out-Null
+}
+
+$PrinterList = Get-Printer
+
+foreach ($Printer in $PrinterList) {
+    $PrinterConfig = Get-PrintConfiguration -PrinterName $printer.Name
+    $PortConfig = Get-PrinterPort -Name $printer.PortName
+    $PrinterProperties = Get-PrinterProperty -PrinterName $printer.Name
+
+    if ($BackupDriver) {
+        $Driver = Get-PrinterDriver -Name $printer.DriverName | ForEach-Object { $_.InfPath; $_.ConfigFile; $_.DataFile; $_.DependentFiles } | Where-Object { $_ -ne $null }
+        $BackupPath = New-Item -Path "$ENV:TEMP\$($printer.name)" -ItemType Directory -Force
+        $Driver | ForEach-Object { Copy-Item -Path $_ -Destination "$ENV:TEMP\$($printer.name)" -Force }
+        Compress-Archive -Path "$ENV:TEMP\$($printer.name)" -DestinationPath "$BackupDirectory\$($printer.name).zip" -Force
+        Remove-Item "$ENV:TEMP\$($printer.name)" -Force -Recurse
+    }
+
+    New-HTML {
+        New-HTMLTab -Name $printer.name {
+            New-HTMLSection -Invisible {
+                New-HTMLSection -HeaderText 'Configuration' {
+                    New-HTMLTable -DataTable $PrinterConfig
+                }
+            }
+            New-HTMLSection -Invisible {
+                New-HTMLSection -HeaderText "Port Config" {
+                    New-HTMLTable -DataTable $PortConfig
+                }
+
+                New-HTMLSection -HeaderText "Printer Properties" {
+                    New-HTMLTable -DataTable $PrinterProperties
+                }
+            }
+        }
+    } -FilePath "$BackupDirectory\$($Printer.name) PrinterDocumentation.html" -Online
+}
+
+Write-Host "Backed up printer config to $BackupDirectory"
